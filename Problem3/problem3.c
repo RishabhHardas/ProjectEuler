08ddc761d5ea0feb1a4b1c1f55a1afd79cfbfeb3
Rishabh Hardas <rishabhhardas@gmail.com>
2018-08-21 21:13:56 +0530

    Problem3 improved (square root)

--- Problem3/problem3.c
+++ Problem3/problem3.c
@@ -6,6 +6,7 @@ int main()
 	long long  n = 600851475143;
 	int factor = 2;
 	int last_factor = 0;
+	long long max_factor = 0;
 
 	if (n % 2 == 0) {
 		n = n/2;
@@ -19,18 +20,23 @@ int main()
 		last_factor = 1;
 
 	factor = 3;
-
-	while ( n > 1) {
+	max_factor = (long long) sqrt(n);
+	//printf("Max Factor = %d\n", max_factor);
+	while (( n > 1) || (factor <= max_factor)) {
 		if (n % factor == 0) {
 			last_factor = factor;
 			n = n / factor;
 			while (n % factor == 0) {
 				n = n / factor;
 			}
+
+			max_factor = (long long) sqrt(n);
 		}
 
-		factor = factor + 1;
+		factor = factor + 2;
 	}
-
-	printf("Largest prime factor = %d\n", last_factor);
+	if (n == 1)
+		printf("Largest Prime Factor = %d\n", last_factor);
+	else
+		printf("Largest prime factor = %lld\n", n);
 }
